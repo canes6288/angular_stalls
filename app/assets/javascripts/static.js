4dc7a3265f3b7d72c70b6f2a3cdb8de136b053cb
canes6288 <joshkushner1@gmail.com>
2014-11-13 10:31:48 -0500

    included Services and cancel functionality

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,45 +1,94 @@
 angular.module('leatherLaneMarketApp', ['ngAnimate'])
- .controller('MarketController', ["$http","$scope", function($http, $scope){
-   $scope.stalls = [];
+.service('stallService', function($http) {
 
-   $http.get('/stalls.json').success(function(data){
-    $scope.stalls = data;
-   })
+  this.getAll = function() {
+    return $http.get('/stalls.json');
+  };
 
+  this.add = function(stall) {
+    return $http.post('/stalls.json', { stall : stall });
+  };
 
+  this.update = function(stall) {
+    return $http.put('/stalls/' + stall.id + '.json', { stall: stall });
+  };
 
-   $scope.selectStall= function(stall) {
+  this.delete = function(stall) {
+    return $http.delete('/stalls/' + stall.id + '.json');
+  };
+
+})
+.controller('MarketController',
+            ["$scope", "stallService",
+            function($scope, stallService) {
+
+  $scope.stalls = [];
+
+  $scope.getStalls = function() {
+    stallService.getAll().success(function(data) {
+      $scope.stalls = data;
+    }).error(function() {
+      alert('Something went wrong!');
+    });
+  };
+
+  $scope.getStalls();
+
+  $scope.newStall = null;
+
+  $scope.selectStall = function(stall) {
     $scope.selectedStall = stall;
-   };
+  };
 
-   $scope.clearSelectedStall = function() {
-    $scope.selectedStall = false;
-   };
+  $scope.clearSelectedStall = function() {
+    $scope.selectedStall = null;
+  };
 
   $scope.addStall = function() {
-    $http.post('/stalls.json', { stall: $scope.newStall }).success(function(data){
-    $scope.stalls.push(data);
-    $scope.newStall = false;
-    $scope.stallForm.$setPristine();
+    stallService.add($scope.newStall)
+    .success(function(data) {
+      $scope.stalls.push(data);
+      $scope.newStall = null;
+      $scope.stallForm.$setPristine();
+    }).error(function() {
+      alert('Something went wrong!');
     });
-   };
+  };
 
-   $scope.deleteStall = function(stall) {
-    $http.delete('/stalls/' + stall.id + '.json').success(function(data){
+  $scope.deleteStall = function(stall) {
+    stallService.delete(stall)
+    .success(function(data) {
+      // $scope.getStalls();
       $scope.stalls.splice($scope.stalls.indexOf(stall), 1);
       $scope.selectedStall = null;
     });
-   };
+  };
 
-   $scope.setEditStall = function(stall){
-    $scope.editStall = stall;
-   };
+  $scope.setEditStall = function(stall) {
+    $scope.editStall = angular.copy(stall);
+  };
 
-   $scope.updateStall = function(stall){
-    $http.put('/stalls/' + stall.id + '.json', { stall: stall }).success(function(data){
-      $scope.editStall = null;
+  $scope.clearEditStall = function() {
+    $scope.editStall = null;
+  };
+
+  $scope.updateStall = function(stall) {
+    stallService.update(stall)
+    .success(function() {
+      angular.copy($scope.editStall, $scope.selectedStall);
+      $scope.clearEditStall();
     });
-   };
+  };
 
-
- }]);
+  $scope.searchFunction = function(stall) {
+    if (!$scope.search) {
+      return true;
+    }
+    // case insensitive filtering
+    if (stall.name.toUpperCase().indexOf($scope.search.toUpperCase()) != -1
+     || stall.description.toUpperCase().indexOf($scope.search.toUpperCase()) != -1) {
+      return true;
+    }
+    return false;
+  };
+}]);
